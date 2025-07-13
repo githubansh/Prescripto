21abe09ee71fdb32699f97132a5453a7a96d620c
githubansh <gadwalansh@gmail.com>
2025-07-13 15:22:38 +0530

    appointement-docid

--- Frontend/src/pages/Appointment.jsx
+++ Frontend/src/pages/Appointment.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
+
 
 const Appointement = () => {
+
+  const {docID}= useParams()
+  const {doctors}= useContext(AppContext)
+
+
+  const fetchDocinfo=async()=>
   return (
     <div>Appointement</div>
   )
