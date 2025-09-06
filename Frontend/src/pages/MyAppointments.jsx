e4bbb03cf869d1d66110080125c11b8284b9110d
githubansh <153741483+githubansh@users.noreply.github.com>
2025-09-06 18:15:00 +0530

    Update MyAppointments.jsx

--- Frontend/src/pages/MyAppointments.jsx
+++ Frontend/src/pages/MyAppointments.jsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react'
 import Appointment from './Appointment'
 import { toast } from 'react-toastify'
 import { useNavigate } from 'react-router-dom'
-import {assets} from '../asseets/assets'
+import {assets} from '../assets/assets'
 
 const MyAppointments = () => {
 
